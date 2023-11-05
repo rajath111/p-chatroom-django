0422f86c3aa9fb094aee4429a7702b9c47a06329
Rajath E S <rajathes999@gmail.com>
2023-11-05 11:57:25 +0530

    Used Async Web socket consumer

--- src/core/chat/consumers.py
+++ src/core/chat/consumers.py
@@ -1,45 +1,40 @@
-from channels.generic.websocket import WebsocketConsumer
-from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 import random
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.user_name = str(random.randint(1, 10000))
-        # Get room name from URL
+        
+        # Get room name from URL, Create chat room name with suffix to room name
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-
-        # Create chat room name with suffix to room name
         self.room_group_name = f'chat_{self.room_name}'
 
         # Added the channel to the group, with group name is same as room name
-        async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
         # Accect the socket
-        self.accept()
-        
-        self.send(json.dumps({'message': 'username: ' + self.user_name}))
+        await self.accept()
+
+        await self.send(json.dumps({'message': 'username: ' + self.user_name}))
 
     
-    def disconnect(self, code):
-        async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
         return super().disconnect(code)
 
 
     # Receives message from Web socket
-    def receive(self, text_data=None, bytes_data=None):
+    async def receive(self, text_data=None, bytes_data=None):
         message = json.loads(text_data)['message']
         print(self.scope['user'])
 
         # Consumer should have equivalent method to the type passed
-        async_to_sync(self.channel_layer.group_send)(self.room_group_name, {"type": "chat.message", "message": f"{self.user_name} : {message}"})
-        
-        # Sends message back to User
-        # self.send(text_data=json.dumps({'message': message}))
+        await self.channel_layer.group_send(self.room_group_name, {"type": "chat.message", "message": f"{self.user_name} : {message}"})
 
 
     # This receives message from Channel group
-    def chat_message(self, event):
-        self.send(text_data=json.dumps({'message': event['message']}))
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps({'message': event['message']}))
 
         
